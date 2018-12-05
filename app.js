37ca1302400e99f3f28b0ad846857e6095d4de9f
Nancy <butlern0513@gmail.com>
2018-12-05 13:18:58 -0700

    nailed it

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-var fs = require('fs');
+ var fs = require('fs');
+//
+// //challenge 1
+// console.log(fs.readFileSync('challenge1/info.txt', 'utf-8'));
+//
+// //challenge 2
+// fs.writeFileSync('challenge2/info.txt', 'Cindy\nDavid\nEdwin\nGreg\nJun\nRico\nMandy\nNancy\nClarence\nCT')
+//
+// //challenge 3
+// // fs.copyFileSync('challenge3/binfo.txt', 'challenge3/info.txt')
+// // fs.unlinkSync('challenge3/binfo.txt')
+//
+// //challenge 4
+// fs.mkdirSync('challenge4/copyfolder')
+// fs.copyFileSync('challenge4/info.txt', 'challenge4/copyfolder/info.txt')
+
+//challenge 5
+// fs.writeFileSync('challenge5/info.txt', fs.readFileSync('challenge5/info.txt', 'utf-8').replace(/-/g, " "));
+
+//challenge 6
+fs.readdirSync('challenge6', {withFileTypes: true}).map((file)=>{
+  if(file.includes('txt')) console.log(file)
+})
